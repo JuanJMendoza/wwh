ed57c89b9946d20d33825fa2a13d56fb06ad21f6
Juan Mendoza <juanmendoza@Juans-MBP.fios-router.home>
2022-09-17 19:10:53 -0400

    let there be light!

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.get("/", (req, res) => {
+  // res.sendFile(path.join(__dirname, "/index.html"));
+  res.redirect("https://mailchi.mp/e1d053fc2493/world-wide-handball");
+});
+
+app.listen(3000);
