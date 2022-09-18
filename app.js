6a317c3f5700c9d6faaf225baca76c1e6eb08146
Juan Mendoza <juanmendoza@Juans-MBP.fios-router.home>
2022-09-18 12:21:02 -0400

    fix redirect to mailchimp fr this time

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
+const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => {
-  // res.sendFile(path.join(__dirname, "/index.html"));
-  res.statusCode = 302;
-  res.setHeader(
-    "Location",
-    "https://mailchi.mp/e1d053fc2493/world-wide-handball"
-  );
-  res.end();
+  //   res.sendFile(path.join(__dirname, "/index.html"));
+  res.redirect("https://mailchi.mp/e1d053fc2493/world-wide-handball");
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, () => {
+  console.log(`Sever is live on port ${PORT}`);
+});
